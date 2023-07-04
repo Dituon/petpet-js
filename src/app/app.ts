4f37dc62b8c9a6a96635e4ca94f366eefcf19388
Dituon <68615161+Dituon@users.noreply.github.com>
2023-07-04 18:30:20 +0900

    move file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import config from "../config.js"
+import config from "../../config.js"
 import {TemplateSelector} from "./template-selector"
 import {AvatarUploader} from "./uploader";
 import {ResultArea} from "./result-area/result-area.js";
