c28df69a4aec4f8792b7a6c478f1037b64698947
Dituon <68615161+Dituon@users.noreply.github.com>
2023-07-16 18:55:30 +0900

    fix static path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 export default defineConfig({
     base: './',
     build: {
         outDir: './dist'
-    }
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [{
+                src: './data',
+                dest: './'
+            }]
+        })
+    ]
 })
