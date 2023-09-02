3c29c4399155190869784a122bf910c4eccfb95b
Dituon <Dituon@users.noreply.github.com>
2023-09-02 11:01:29 +0000

    deploy: 32357e00a7634979e6d69a557acf3987fca19e86

--- inject.js
+++ inject.js
@@ -1,7 +1,8 @@
-const scriptUrl = new URL(document.currentScript.src)
-const rootUrl = scriptUrl.origin + scriptUrl.pathname
+;(() => {
+    const scriptUrl = new URL(document.currentScript.src)
+    const rootUrl = scriptUrl.origin + scriptUrl.pathname
 
-const cssText = `
+    const cssText = `
 :root {
     --petpet-icon-size-def: 3.6em;
     --petpet-body-width-def: 28em;
@@ -32,19 +33,52 @@ const cssText = `
 }
 `
 
-const style = document.createElement('style')
-style.innerText = cssText
-document.head.appendChild(style)
+    const style = document.createElement('style')
+    style.innerText = cssText
+    document.head.appendChild(style)
 
-const iframe = document.createElement('iframe')
+    const iframe = document.createElement('iframe')
 
-iframe.classList.add('petpet-iframe', 'petpet-hide');
-iframe.src = new URL('./index.html', rootUrl).toString()
+    iframe.classList.add('petpet-iframe', 'petpet-hide');
+    iframe.src = new URL('./index.html', rootUrl).toString()
 
-const logo = document.createElement('div')
-logo.classList.add('petpet-icon')
-logo.addEventListener('click', () => {
-    iframe.classList.toggle('petpet-hide')
-})
-document.body.appendChild(logo)
-document.body.appendChild(iframe)
+    const icon = document.createElement('div')
+    icon.classList.add('petpet-icon')
+    document.body.appendChild(icon)
+    document.body.appendChild(iframe)
+
+
+    let isDragging = false
+    const offset = {x: 0, y: 0}
+    const mPos = {x: 0, y: 0}
+
+    const startDragging = e => {
+        isDragging = true
+        e.preventDefault()
+        const {left, top, width, height} = icon.getBoundingClientRect()
+        offset.x = e.clientX - (left + width / 2);
+        offset.y = e.clientY - (top + height / 2);
+        mPos.x = e.clientX
+        mPos.y = e.clientY
+    }
+
+    const stopDragging = e => {
+        isDragging = false
+        icon.style.cursor = 'pointer'
+        if (Math.abs(mPos.x - e.clientX) < 10 && Math.abs(mPos.y - e.clientY) < 10) {
+            iframe.classList.toggle('petpet-hide')
+        }
+    }
+
+    const drag = (e) => {
+        if (isDragging) {
+            icon.style.cursor = 'grabbing'
+            icon.style.left = `${e.clientX - offset.x - icon.offsetWidth / 2}px`;
+            icon.style.top = `${e.clientY - offset.y - icon.offsetHeight / 2}px`;
+        }
+    }
+
+    icon.addEventListener('mousedown', startDragging)
+    icon.addEventListener('mouseup', stopDragging)
+    document.body.addEventListener('mousemove', drag)
+})();
