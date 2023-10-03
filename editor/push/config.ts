b30df7a1e38bfa943c7f87d96524be46d1e25311
Dituon <68615161+Dituon@users.noreply.github.com>
2023-10-03 15:17:09 +0900

    Fix: Editor: config CLIENT_ID

--- editor/push/config.ts
+++ editor/push/config.ts
@@ -1,9 +1,9 @@
-export const CLIENT_ID = process.env.CLIENT_ID
+export const CLIENT_ID = '65b3c999db80ac568989'
 
 export const GITHUB_API_URL = 'https://api.github.com'
 
-export const OAUTH_PROXY_URL = process.env.OAUTH_PROXY_URL
+export const OAUTH_PROXY_URL = 'https://d2n.moe/github/oauth'
 
-export const REDICRECT_URI = process.env.REDICRECT_URI
+export const REDICRECT_URI = location.href
 
 export const urlParam = new URL(location.href).searchParams
